4be99fe4bef7037fc1da96b4a7f401c42c064cfb
CodingBody <ShawnCoding21@gmail.com>
2019-12-26 17:25:26 +0900

    updating our app with a shop page, collection preview component and collection item component

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -45,8 +45,8 @@ class Directory extends Component {
 
     return (
       <div className="directory-menu">
-        {sections.map(({ title, imageUrl, id, size }) => (
-          <MenuItem key={id} title={title} size={size} imageUrl={imageUrl} />
+        {sections.map(({ id, ...otherSectionProps }) => (
+          <MenuItem key={id} {...otherSectionProps} />
         ))}
       </div>
     );
