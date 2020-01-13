4e7e0e49c446a9ac1e2a15fdc6f245487bab9faf
CodingBody <ShawnCoding21@gmail.com>
2020-01-13 16:00:22 +0900

    update

--- client/src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ client/src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -2,9 +2,6 @@ import React from "react";
 import "./sign-in-and-sign-up.styles.scss";
 import SignIn from "../../components/sign-in/sign-in.component";
 import SignUp from "../../components/sign-up/sign-up.component";
-import { connect } from "react-redux";
-import { createStructuredSelector } from "reselect";
-import { selectSignInAndSignUpError } from "../../redux/user/user.selector";
 
 const SignInAndSignUpPage = () => {
   return (
