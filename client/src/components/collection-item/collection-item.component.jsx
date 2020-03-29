914dd4e78fd2595650ba84095933df7954bf4e0b
CodingBody <ShawnCoding21@gmail.com>
2020-03-29 17:07:08 +0900

    removed div and added img to be recognized by  SEO

--- client/src/components/collection-item/collection-item.component.jsx
+++ client/src/components/collection-item/collection-item.component.jsx
@@ -11,12 +11,7 @@ const CollectionItem = ({ history, currentUser, addItem, item }) => {
   const { name, price, imageUrl } = item;
   return (
     <div className="collection-item">
-      <div
-        className="image"
-        style={{
-          backgroundImage: `url(${imageUrl})`
-        }}
-      />
+      <img className="image" src={imageUrl} alt="clothes" />
       <div className="collection-footer">
         <span className="name">{name}</span>
         <span className="price"> {price} </span>
